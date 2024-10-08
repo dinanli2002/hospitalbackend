4ba159d2ea94b5e460963ba87b3ab6aa5c018915
dinanli2002 <dinan.li2003@gmail.com>
2024-10-08 09:49:51 +0200

    first commit

---
+++ src/main/java/com/example/Introduccionbackend/IntroduccionbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Introduccionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroduccionbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroduccionbackendApplication.class, args);
+	}
+
+}
