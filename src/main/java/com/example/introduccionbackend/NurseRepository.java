f74209709c8551704c02576acd031e2361c72af3
dinanli2002 <dinan.li2003@gmail.com>
2024-11-02 11:26:36 +0100

    issues #29

--- src/main/java/com/example/introduccionbackend/NurseRepository.java
+++ src/main/java/com/example/introduccionbackend/NurseRepository.java
@@ -1,7 +1,6 @@
 package com.example.introduccionbackend;
 
 import java.util.Optional;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NurseRepository extends JpaRepository<Nurse, Integer>{
